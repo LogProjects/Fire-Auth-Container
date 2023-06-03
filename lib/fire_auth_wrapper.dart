0f775d9892cf7fd21a32ca4c03acf8d16635be18
erol <logaritma26@protonmail.com>
2023-06-03 12:51:18 +0200

    implementation

--- lib/fire_auth_wrapper.dart
+++ lib/fire_auth_wrapper.dart
@@ -1,7 +1,9 @@
 library fire_auth_wrapper;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/cubit/auth_cubit.dart';
+export 'src/cubit/auth_notifier.dart';
+export 'src/data/auth_repository_impl.dart';
+export 'src/domain/auth_repository.dart';
+export 'src/model/auth_cubit_observable.dart';
+export 'src/model/auth_state.dart';
+export 'src/model/auth_view_state.dart';
